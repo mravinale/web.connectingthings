8e67c66b882a04cc9a8ec27280de647986922990
Mariano Ravinale <mravinale@gmail.com>
2016-04-18 23:37:01 -0300

    improved sections

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -382,6 +382,25 @@ function onePageScroll() {
           //I get fired when you enter a section and I pass the list item of the section
       }
   });
+
+  $('.internal-nav').onePageNav({
+    currentClass: 'current',
+    changeHash: false,
+    scrollSpeed: 650,
+    scrollOffset: 30,
+    scrollThreshold: 0.5,
+    filter: ':not(.login, .signup)',
+    easing: 'swing',
+    begin: function() {
+      //I get fired when the animation is starting
+    },
+    end: function() {
+      //I get fired when the animation is ending
+    },
+    scrollChange: function($currentListItem) {
+      //I get fired when you enter a section and I pass the list item of the section
+    }
+  });
 }
 
 
